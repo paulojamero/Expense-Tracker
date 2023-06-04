890c131419e03d071004c89919ebd1e3766a4b1f
paulojamero <jameropaulo07@yahoo.com>
2023-06-04 16:05:48 +0800

    Add Transaction function
    1. Add Transaction Controller
    2. Edit transaction form

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -10,7 +10,7 @@ namespace Expense_Tracker.Models
 
         //Category Id - Foreign key
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
